f21c162af9ee394bf8bb30fa24a96c48acf9bb48
bhupender <bhupender.cool98@gmail.com>
2022-01-27 18:48:37 +0530

    code structure

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /*
-<script languae="javascript"/>->old broswer
-<script type="text/javascript"/>it is ued in html4 
-<sript src =""/>
-<sript/> -=> in htlm 5 you can ignore type
 
+Statement
+Semicolons
+single line Commetns ctrl+/ for windows for mac=cmd+/
+multiple lines Comments  ctrl+shift+/ for windows for mac=cmd+option+/
+js doen't assume a semicolon before the sqare []
 */
-console.log("Hello,Addictive Learning");
+//console.log("Hello Bhupender");
+["bhupender", "sonu", "ashish"].map((e) => console.log(e));
