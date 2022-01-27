46d5a8588882a2824de1f60c21d9575b83a3d30e
bhupender <bhupender.cool98@gmail.com>
2022-01-27 17:23:54 +0530

    script tag

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+<script languae="javascript"/>->old broswer
+<script type="text/javascript"/>it is ued in html4 
+<sript src =""/>
+<sript/> -=> in htlm 5 you can ignore type
+
+*/
+console.log("Hello,Addictive Learning");
